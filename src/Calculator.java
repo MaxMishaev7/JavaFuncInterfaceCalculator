b3db84a82b7b50bc645b4d5d9331c3402bd36f80
MaxMishaev <maxmishaev74@gmail.com>
2024-02-12 18:08:09 +0300

    Calculator.java changed

--- src/Calculator.java
+++ src/Calculator.java
@@ -10,8 +10,13 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> divide = (x, y) -> (y != 0) ? x / y  : 0;
-
+    BinaryOperator<Integer> divide = (x, y) -> {if (y != 0) {
+                                                  return x / y;
+                                                } else {
+                                                  System.out.println("Division By Zero");
+                                                  return 0;
+                                                }
+                                                };
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
 
